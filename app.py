d7538fe6a96a580d0b241c7dcc87231d63dd1ad2
Dabidesu <davidezekielgarin@gmail.com>
2023-11-08 14:36:04 +0800

    a
    a

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, url_for, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home/')
+def index():
+    return render_template('index.html') 
+
+@app.route('/contact-us/')
+def contact_page():
+    return render_template('contact-us.html') 
+
+if __name__ == '__main__':
+    app.run(debug=True)
