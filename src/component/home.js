f2e83b28a368eca28fd9c8cba62268fc06744c1c
Dabidesu <davidezekielgarin@gmail.com>
2023-11-22 07:30:39 +0800

    a
    a

--- src/component/home.js
+++ src/component/home.js
@@ -1,7 +1,14 @@
 import React from 'react'; 
+import ReactDOM from 'react-dom';
+// import Dashboard from 'app-b-dashboard/container' 
+// import Page from '../../public/index.html';
+
+
 
 function Home (){ 
-    return <h1>Homeo</h1> 
+    const container = document.getElementById('root');
+    const root = ReactDOM.createRoot(container);
+    return ReactDOM.render(process.env.PUBLIC_URL, document.getElementById('root'));
 } 
   
 export default Home;
