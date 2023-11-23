ac908baf39cc40a832339df3f7ea03a5a5c08ab1
Dabidesu <davidezekielgarin@gmail.com>
2023-11-23 09:24:27 +0800

    a
    a

--- src/App.js
+++ src/App.js
@@ -43,7 +43,8 @@ render() {
                 <Route exact path='/about' element={< About />}></Route> 
                 <Route exact path='/contact-us' element={< Contact />}></Route> 
                 <Route exact path='/contact-us/' element={< Contact />}></Route> 
-                <Route path='*' element={<Navigate to='/' />} />
+                <Route path='/contact-us/' element={<Navigate to='/contact-us' />}></Route> 
+                <Route path='*' element={<Navigate to='/' />}></Route> 
         </Routes> 
     </Router> 
 ); 
